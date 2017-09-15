3663d12a0122b077eb8c8c18f5dc4a1309441c4d
Andres <af.mape@uniandes.edu.co>
2017-09-15 10:32:43 -0500

    Se quito la implementacion del servicio de mensaje

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -29,7 +29,6 @@ export const routes: Routes = [
             },
         ]
     }
-
 ];
 
 @NgModule({
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,6 @@
 import { Subscription } from 'rxjs/Subscription';
 import { Component, OnDestroy } from '@angular/core';
-import { MessageService } from "./message.service";
+
 
 @Component({
   selector: 'app-root',
@@ -9,17 +9,4 @@ import { MessageService } from "./message.service";
 })
 
 export class AppComponent { }
-// export class AppComponent implements OnDestroy {
-//   message: any;
-//   subscription: Subscription;
 
-//   constructor(private messageService: MessageService) {
-
-//     // subscribe to home component messages
-//     this.subscription = this.messageService.getMessage().subscribe(message => { this.message = message; });
-//   }
-
-//   ngOnDestroy() {
-//     // unsubscribe to ensure no memory leaks
-//     this.subscription.unsubscribe();
-//   }
